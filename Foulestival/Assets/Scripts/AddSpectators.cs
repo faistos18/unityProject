be3a28a2c3e42c8c5233700c0a5eaec9839619a8
faistos18 <mathieu.galle@ensc.fr>
2015-11-16 14:50:54 +0100

    AJout de barrieres, mouvements de caméra, navmesh, multiples agents.

---
+++ Foulestival/Assets/Scripts/AddSpectators.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddSpectators : MonoBehaviour {
+
+
+	public Transform spectator;
+	public Transform arrivalZone;
+    public float arrivalZoneSize;
+    private int numberOfSpectators;
+
+	// Use this for initialization
+	void Start ()
+    {
+        numberOfSpectators = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(spectator, new Vector3(arrivalZone.position.x + (Random.value * 2 - 1) * arrivalZoneSize, 0, arrivalZone.position.z + (Random.value * 2 - 1)) * arrivalZoneSize, Quaternion.identity);
+            numberOfSpectators++;
+        }
+	}
+
+}
--- Foulestival/Assets/Scripts/StrategyGameCamera.cs
+++ Foulestival/Assets/Scripts/StrategyGameCamera.cs
@@ -5,6 +5,7 @@ public class StrategyGameCamera : MonoBehaviour {
 
     [Range(0.1f,20)]
     public float cameraSpeed;
+    public float cameraRotatationSpeed;
 
     public float minHeight;
     public float maxHeight;
@@ -48,11 +49,12 @@ public class StrategyGameCamera : MonoBehaviour {
                 this.transform.Translate(new Vector3(0, 0, Input.mouseScrollDelta.y * zoomSpeed * Time.deltaTime)); //Translates relative to the camera. For zooming in.
         }
 	}
-
     public Vector3 GetDesiredTranslation()
     {
         Vector3 tr = new Vector3(0, 0, 0);
         Vector3 m = Input.mousePosition;
+
+        float rotation =0;
 
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q) || (m.x < ScrollLimits.LeftBorder && m.x > -5))
             tr.x -= 1.0f;
@@ -65,11 +67,16 @@ public class StrategyGameCamera : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) || ( m.y < ScrollLimits.DownBorder && m.y > -5)  )
             tr.z -= 1.0f;
+        if (Input.GetKey(KeyCode.A))
+            rotation-=1;
+        if (Input.GetKey(KeyCode.E))
+            rotation+=1;
 
         tr *= Time.deltaTime;
         tr *= cameraSpeed;
 
         tr *= this.transform.position.y; //The more high we are (...), the more the speed is.
+        this.transform.Rotate(new Vector3(0,1,0),rotation*cameraRotatationSpeed*Time.deltaTime,Space.World);
         return tr;
     }
 
@@ -83,7 +90,9 @@ public class StrategyGameCamera : MonoBehaviour {
             Input.GetKey(KeyCode.UpArrow) ||
             Input.GetKey(KeyCode.DownArrow) ||
             Input.GetKey(KeyCode.RightArrow) ||
-            Input.GetKey(KeyCode.LeftArrow));
+            Input.GetKey(KeyCode.LeftArrow) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.E) );
     }
     public static bool IsMouseOutBorders()
     {
