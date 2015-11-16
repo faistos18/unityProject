44cf0b1354234252c3668c899aa5e75b2e6e71f1
faistos18 <mathieu.galle@ensc.fr>
2015-11-16 08:57:40 +0100

    J'ai ajouté tous les fichiers, peut-être trop.

---
+++ Foulestival/Assets/Scripts/ClickToGo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToGo : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    // Use this for initialization
+    void Start()
+    {
+        navMeshAgent = (NavMeshAgent)gameObject.GetComponent("NavMeshAgent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))//1 is right click, 0 is left, 2 is middle.
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 1000000000000))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+    }
+
+}
